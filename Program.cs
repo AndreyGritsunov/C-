33ffb76092a0f920cc2c02279536a659db47b881
unknown <gritsunov85@gmail.com>
2022-08-09 22:35:19 +0600

    DZ1

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число:");
+int number_one = Convert.ToInt32(Console.Read());
+Console.WriteLine("Введите число2:");
+int number_two = Convert.ToInt32(Console.Read());
+
+if(number_one > number_two) Console.Write(number_one);
+if(number_one < number_two) Console.Write(number_two);
+if(number_one == number_two) Console.Write("Числа одинаковы");
