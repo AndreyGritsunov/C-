c0c27077f0c780132b001b80663f4155b9f0f885
unknown <gritsunov85@gmail.com>
2022-08-09 23:07:53 +0600

    DZ1

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Введите число:");
-int number_one = Convert.ToInt32(Console.Read());
+int number_one = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число2:");
-int number_two = Convert.ToInt32(Console.Read());
+int number_two = Convert.ToInt32(Console.ReadLine());
 
 if(number_one > number_two) Console.Write(number_one);
 if(number_one < number_two) Console.Write(number_two);
